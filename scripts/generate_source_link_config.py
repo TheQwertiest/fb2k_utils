87817ebf86b7ac9334f834731ddc4619f3b80120
TheQwertiest <qwertiest@mail.ru>
2020-10-03 02:17:38 +0300

    Updated generation scripts

--- scripts/generate_source_link_config.py
+++ scripts/generate_source_link_config.py
@@ -3,6 +3,7 @@
 import argparse
 import json
 import subprocess
+import os
 from pathlib import Path
 from pathlib import PureWindowsPath
 from typing import Union
@@ -11,15 +12,28 @@ import call_wrapper
 
 PathLike = Union[str, Path]
 
-def generate_config_custom( base_dir: PathLike,
+def get_cwd_repo_root():
+    repo_dir = Path(os.getcwd()).absolute()
+    cmd_get_root = [
+        'git',
+        '-C',
+        str(repo_dir),
+        'rev-parse',
+        '--show-toplevel'
+    ]
+
+    root = subprocess.check_output(cmd_get_root, text=True, env=os.environ, cwd=repo_dir).strip()
+    return Path(root)
+
+def generate_config_custom( repo_dir: PathLike,
                             output_dir: PathLike,
                             repo: str,
                             commit_hash: str = ""):
     if (not commit_hash):
         commit_hash = subprocess.check_output("git rev-parse --short HEAD", shell=True).decode('ascii').strip()
 
-    base_dir = Path(base_dir).resolve()
-    assert(base_dir.exists() and base_dir.is_dir())
+    repo_dir = Path(repo_dir).resolve()
+    assert(repo_dir.exists() and repo_dir.is_dir())
 
     output_dir = Path(output_dir).resolve()
     output_dir.mkdir(parents=True, exist_ok=True)
@@ -28,7 +42,7 @@ def generate_config_custom( base_dir: PathLike,
     output_file.unlink(missing_ok=True)
 
     data = {}
-    data['documents'] = { f'{PureWindowsPath(base_dir)}*': f'https://raw.githubusercontent.com/{repo}/{commit_hash}/*' }
+    data['documents'] = { f'{PureWindowsPath(repo_dir)}*': f'https://raw.githubusercontent.com/{repo}/{commit_hash}/*' }
 
     with open(output_file, 'w') as output:
         json.dump(data, output)
@@ -36,12 +50,10 @@ def generate_config_custom( base_dir: PathLike,
     print(f"Generated file: {output_file}")
 
 if __name__ == '__main__':
-    cur_dir = Path(__file__).parent.absolute()
-    root_dir = cur_dir.parent
-    output_dir = root_dir/"_result"/"AllPlatforms"/"generated"
+    repo_dir = get_cwd_repo_root()
+    output_dir = repo_dir/"_result"/"AllPlatforms"/"generated"
 
     parser = argparse.ArgumentParser(description='Generate source link configuration file')
-    parser.add_argument('--base_dir', default=root_dir)
     parser.add_argument('--output_dir', default=output_dir)
     parser.add_argument('--repo')
     parser.add_argument('--commit_hash', default="")
@@ -55,7 +67,7 @@ if __name__ == '__main__':
     )(
     generate_config_custom
     )(
-        args.base_dir,
+        repo_dir,
         args.output_dir,
         args.repo,
         args.commit_hash
