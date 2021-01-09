ac5cb562085b0f831b94f56bdfbe6a7bcd119eb8
TheQwertiest <qwertiest@mail.ru>
2021-01-10 00:40:09 +0300

    Fixed fb2k adv config wrapper

--- src/qwr/fb2k_adv_config.h
+++ src/qwr/fb2k_adv_config.h
@@ -21,14 +21,14 @@ public:
 
     AdvConfig( const AdvConfig& ) = delete;
 
-    operator InnerT() const
+    operator value_type() const
     {
         return GetValue();
     }
 
-    InnerT GetValue() const
+    value_type GetValue() const
     {
-        if constexpr ( std::is_same_v<InnerT, std::string> )
+        if constexpr ( std::is_same_v<value_type, std::string> )
         {
             pfc::string8_fast value;
             config_.get( value );
@@ -36,8 +36,15 @@ public:
         }
         else
         {
-            return config_.get();
+            return static_cast<value_type>( config_.get() );
         }
+    }
+    
+    template <typename ArgT, std::enable_if_t<std::is_convertible_v<ArgT, value_type>, int> = 0>
+    AdvConfig& operator=( const ArgT& value )
+    {
+        config_ = value;
+        return *this;
     }
 
 private:
@@ -62,24 +69,34 @@ public:
 
     AdvConfig_MT( const AdvConfig_MT& ) = delete;
 
-    operator InnerT() const
+    operator value_type() const
     {
         std::mutex mutex_;
         return config_;
     }
 
-    InnerT GetValue() const
+    value_type GetValue() const
     {
         std::mutex mutex_;
         return config_.GetValue();
     }
 
+    template <typename ArgT, std::enable_if_t<std::is_convertible_v<ArgT, value_type>, int> = 0>
+    AdvConfig_MT& operator=( const ArgT& value )
+    {
+        std::mutex mutex_;
+        config_ = value;
+        return *this;
+    }
+
 private:
     mutable std::mutex mutex_;
-    mutable AdvConfig<T, InnerT> config_;
+    mutable AdvConfig<T, value_type> config_;
 };
 
 using AdvConfigString_MT = AdvConfig<advconfig_string_factory_MT, std::string>;
 using AdvConfigBool_MT = AdvConfig_MT<advconfig_checkbox_factory, bool>;
+using AdvConfigUint8_MT = AdvConfig_MT<advconfig_integer_factory, uint8_t>;
+using AdvConfigUint32_MT = AdvConfig_MT<advconfig_integer_factory, uint32_t>;
 
 } // namespace qwr::fb2k
