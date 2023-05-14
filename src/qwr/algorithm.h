2fa90371e61d6d9768ce7539df31a5160b9a7f27
TheQwertiest <qwertiest@mail.ru>
2023-05-15 02:36:10 +0400

    Fixed bug in FindAsPointer

--- src/qwr/algorithm.h
+++ src/qwr/algorithm.h
@@ -5,19 +5,19 @@
 namespace qwr
 {
 
-template<typename T, typename V>
+template <typename T, typename V>
 auto FindAsPointer( T& map, const V& value )
 {
     auto it = map.find( value );
-    return ( it == map.end() ? &it->second : nullptr );
+    return ( it != map.end() ? &it->second : nullptr );
 }
 
 template <typename T, typename V>
 auto FindAsOptional( T& map, const V& value )
 {
-    std::optional < typename std::remove_cvref_t<T>::mapped_type > ret;
-    if ( auto it = map.find( value ); 
-        it != map.end() )
+    std::optional<typename std::remove_cvref_t<T>::mapped_type> ret;
+    if ( auto it = map.find( value );
+         it != map.end() )
     {
         ret = it->second;
     }
