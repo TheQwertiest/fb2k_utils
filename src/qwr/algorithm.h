bb6c29df351c3cf5c6b013e854f9b8693b696105
TheQwertiest <theqwertiest@outlook.com>
2023-11-07 23:46:38 +0100

    Added FindOrDefault

--- src/qwr/algorithm.h
+++ src/qwr/algorithm.h
@@ -5,23 +5,32 @@
 namespace qwr
 {
 
-template <typename T, typename V>
-auto FindAsPointer( T& map, const V& value )
+template <typename T, typename K>
+auto FindAsPointer( T& map, const K& key )
 {
-    auto it = map.find( value );
+    auto it = map.find( key );
     return ( it != map.end() ? &it->second : nullptr );
 }
 
-template <typename T, typename V>
-auto FindAsOptional( T& map, const V& value )
+template <typename T, typename K>
+auto FindAsOptional( T& map, const K& key )
 {
     std::optional<typename std::remove_cvref_t<T>::mapped_type> ret;
-    if ( auto it = map.find( value );
+    if ( auto it = map.find( key );
          it != map.end() )
     {
         ret = it->second;
     }
     return ret;
+}
+
+template <typename T, typename K, typename D>
+auto FindOrDefault( T& map, const K& key, D&& defaultValue )
+{
+    using ValueT = typename std::remove_cvref_t<T>::mapped_type;
+
+    auto it = map.find( key );
+    return ( it != map.end() ? it->second : ValueT{std::forward<D>(defaultValue)} );
 }
 
 } // namespace qwr
